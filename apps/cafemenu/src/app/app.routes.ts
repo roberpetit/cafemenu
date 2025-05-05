5aaac3d81ceef563a1e29312fcddeb4b993eaabf
Roberto Petit de Meurville <rpetit@riu.com>
2025-05-05 19:50:48 -0300

    Fixed paths

--- apps/cafemenu/src/app/app.routes.ts
+++ apps/cafemenu/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Route } from '@angular/router';
-import { ImgMenuComponent } from './pages/img-menu/img-menu.component';
 import { MenuComponent } from './pages/menu/menu.component';
 
 export const appRoutes: Route[] = [
-    { path: '', component: ImgMenuComponent },
-    { path: 'menu', component: ImgMenuComponent },
-    { path: 'menu/:pagina', component: ImgMenuComponent },
+    { path: '', component: MenuComponent },
+    //{ path: 'menu', component: ImgMenuComponent },
+    //{ path: 'menu/:pagina', component: ImgMenuComponent },
     { path: 'cafe', component: MenuComponent },
     { path: 'cafe/:pagina', component: MenuComponent },
+    { path: '*', redirectTo: '', pathMatch: 'full' },
 ];
