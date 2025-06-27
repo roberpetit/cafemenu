1e1e28eff78c207d61f1dee136a9c18bdf0c7d1e
Roberto Petit de Meurville <rpetit@riu.com>
2025-06-27 17:32:00 -0300

    Page 2 and mobile

--- apps/chan/src/app/app.component.ts
+++ apps/chan/src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { Component, inject, WritableSignal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, Router } from '@angular/router';
-import { AuthService } from '@cafemenu-monorepo/monolib';
+import { AuthService, ToolbarIconsComponent } from '@cafemenu-monorepo/monolib';
 import { ThemeService } from '@cafemenu-monorepo/monolib';
 import { Observable, map, shareReplay } from 'rxjs';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   standalone: true,
-  imports: [RouterModule, CommonModule, MatIconModule, MatToolbarModule, MatButtonModule
-],
+  imports: [
+    RouterModule,
+    CommonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    ToolbarIconsComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -21,12 +26,11 @@ export class AppComponent {
   private breakpointObserver = inject(BreakpointObserver);
   title = 'chan';
   theme: WritableSignal<string>;
-  
+
   constructor(
     private readonly themeService: ThemeService,
     public auth: AuthService,
-    private readonly router: Router,
-
+    private readonly router: Router
   ) {
     this.theme = this.themeService.theme;
     console.log('AppComponent initialized with theme:', this.theme());
@@ -40,7 +44,7 @@ export class AppComponent {
     .observe(Breakpoints.Handset)
     .pipe(
       map((result) => {
-        console.log("isHandset: ",  result);
+        console.log('isHandset: ', result);
         return result.matches;
       }),
       shareReplay()
@@ -62,5 +66,4 @@ export class AppComponent {
     this.auth.logout();
     this.router.navigate(['/']);
   }
-  
 }
